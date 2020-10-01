85044e00c5390d301bb156f8b338d32d415e4ba5
yveso <yveso@users.noreply.github.com>
2020-10-01 11:21:13 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+	return (
+		<div className="App">
+			<h1>SMC HD</h1>
+			<p>
+				Cheese and biscuits cheese and wine blue castello. Pepper jack
+				port-salut st. agur blue cheese macaroni cheese boursin jarlsberg
+				camembert de normandie the big cheese. Airedale bavarian bergkase
+				taleggio pepper jack caerphilly jarlsberg bocconcini port-salut.
+				Bavarian bergkase fromage squirty cheese fromage frais bavarian bergkase
+				boursin.
+			</p>
+			<p>
+				Pecorino lancashire caerphilly. Melted cheese paneer when the cheese
+				comes out everybody's happy everyone loves st. agur blue cheese fromage
+				frais cheese strings roquefort. Squirty cheese emmental fromage frais
+				everyone loves swiss hard cheese mascarpone cut the cheese. Camembert de
+				normandie everyone loves feta melted cheese cheesy feet macaroni cheese
+				taleggio fromage frais. Smelly cheese swiss.
+			</p>
+			<iframe
+				width="560"
+				height="315"
+				src="https://www.youtube-nocookie.com/embed/CWzrABouyeE"
+				frameborder="0"
+				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+				allowfullscreen
+			></iframe>
+		</div>
+	);
+}
