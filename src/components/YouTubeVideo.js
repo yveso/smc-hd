8e3e0bb1d7dee684a99753bb4cc0dc28ed52b75f
yveso <yveso@users.noreply.github.com>
2020-10-01 12:08:23 +0200

    initial commit

---
+++ src/components/YouTubeVideo.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function YouTubeVideo({ youTubeId, width = 560, height = 315 }) {
+	return (
+		<iframe
+			title={`YouTube Video ${youTubeId}`}
+			width={width}
+			height={height}
+			src={`https://www.youtube-nocookie.com/embed/${youTubeId}`}
+			frameBorder="0"
+			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+			allowFullScreen
+		></iframe>
+	);
+}
+
+export default YouTubeVideo;
